d42dbb56dd35da05d7d7c090cb6bcd298d685325
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-07-13 18:31:50 -0300

    Criacao das opcoes do menu, e tratamento do valor de entrada de opcao

--- main.dart
+++ main.dart
@@ -1,7 +1,45 @@
+import 'dart:io';
 void main(List<String> args){
-  
+  menu();
 }
-
+/// Menu 
 void menu(){
+  print('==========Menu==========');
+  print('Digite [1] para soma');
+  print('Digite [2] para subtrair');
+  print('Digite [3] para multiplicar');
+  print('Digite [4] para dividir');
+  print('Digite [5] para sair');
+ String? entradaOp = stdin.readLineSync();
   
+ if((double.tryParse(entradaOp!) ?? 0.0) == 5){
+   print('Você digitou 5, o programa será encerrado');
+   exit(0);
+ }else if(int.parse(entradaOp) > 0 && int.parse(entradaOp) < 5){
+   print('Digite o primeiro valor da operação');
+   String? entradaValor1 = stdin.readLineSync();
+   print('Digite o segundo valor da operação');
+   String? entradaValor2 = stdin.readLineSync();
+
+   if(entradaValor1 != null && entradaValor2 != null){
+      if(entradaValor1.isNotEmpty && entradaValor2.isNotEmpty){
+          try{
+            
+          }catch(e){
+
+          }
+      }else{
+        print('ERRO! valor vazio!!! O programa será encerrado');
+        exit(0);
+      }
+   }else{
+    print('ERRO !!! valor nulo. O programa será encerrado !!!');
+    exit(0);
+   }
+
+ }else{
+  print('Você digitou um valor fora da faixa !');
+  print('O programa será encerrado ');
+  exit(0);
+ }
 }
