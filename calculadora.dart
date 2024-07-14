bf2d8ff1cb768da7b90938c6b1db7ab7379ec912
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-07-14 00:19:35 -0300

    função escolha caso

--- calculadora.dart
+++ calculadora.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
+
 class Calculadora{
   double? _op;
   double? _valor1;
   double? _valor2;
 
   Calculadora({required double op,required double valo1,required double valor2}){
-
+    escolha(op: op, valo1: valo1, valor2: valor2);
   }
 
   double? get op => this._op;
@@ -18,5 +20,23 @@ class Calculadora{
   double? get valor2 => this._valor2;
 
   void set valor2(double? valor2) => this._valor2 = valor2;
+
+
+  void escolha({required double op,required double valo1,required double valor2}){
+    switch(op){
+      case 1:
+      break;
+      case 2:
+      break;
+      case 3:
+      break;
+      case 4:
+      break;
+      default:
+      print('ERRO! valor fora da faixa !!');
+      print('Encerrando o programa');
+      exit(0);
+    }
+  }
 
 }
